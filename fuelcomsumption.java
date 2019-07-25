ed8635215ae0db5a06d2bc698e16a0dcf6af19db
Nirawat P <fovexlazy@gmail.com>
2019-07-26 02:20:50 +0700

    fuelconsumption

---
+++ fuelcomsumption.java
@@ -0,0 +1,12 @@
+public class fuelcomsumption {
+    
+    static void main(String[] args) {
+        double distance = 300;
+        double volume = 20;
+        double consumptionRate = ;
+   
+        system.out.print ("this car consumes ");
+        system.out.print ("consumptionRate");
+        system.out.println(" km/litre");
+    }
+   }
